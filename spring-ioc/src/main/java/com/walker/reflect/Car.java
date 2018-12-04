3ad2919d104291c4d03d17566768eca7d6a8f047
walker <zzuiewpp@163.com>
2018-12-04 16:49:57 +0800

    Add ClassLoader test class

---
+++ spring-ioc/src/main/java/com/walker/reflect/Car.java
@@ -0,0 +1,37 @@
+package com.walker.reflect;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author walker
+ * @version 1.0
+ * @since 2018-12-04 15:25
+ */
+
+public class Car {
+    @Getter
+    @Setter
+    private String brand;
+
+    @Getter
+    @Setter
+    private String color;
+
+    @Getter
+    @Setter
+    private Integer maxSpeed;
+
+    public void introduce() {
+        System.out.println(toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                '}';
+    }
+}
---
+++ spring-ioc/src/main/java/com/walker/reflect/ReflectTest.java
@@ -0,0 +1,40 @@
+package com.walker.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+/**
+ * @author walker
+ * @version 1.0
+ * @since 2018-12-04 15:34
+ */
+public class ReflectTest {
+    public static Car initByDefaultConst() throws Throwable {
+        // 通过类加载器获取对象
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        // 以下三个loader存在父子层级关系
+        System.out.println(loader);
+        System.out.println(loader.getParent());
+        System.out.println(loader.getParent().getParent());
+        Class clazz = loader.loadClass("com.walker.reflect.Car");
+
+        // 通过默认的构造器获取实例
+        Constructor constructor = clazz.getDeclaredConstructor((Class[])null);
+        Car car = (Car) constructor.newInstance();
+
+        // 通过反射设置属性
+        Method setBrand = clazz.getMethod("setBrand", String.class);
+        setBrand.invoke(car, "benz");
+        Method setColor = clazz.getMethod("setColor", String.class);
+        setColor.invoke(car, "black");
+        Method setMaxSpeed = clazz.getMethod("setMaxSpeed", Integer.class);
+        setMaxSpeed.invoke(car, 400);
+
+        return car;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Car car = initByDefaultConst();
+        car.introduce();
+    }
+}
